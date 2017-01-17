f7728ec650910b1a1a25c2c4b743844e97ccb7c7
emeraldarcher <cornelison.kylejohn@gmail.com>
2017-01-17 16:25:55 -0700

    Initial commit of working starter project

---
+++ config/webpack.common.js
@@ -0,0 +1,62 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var helpers = require('./helpers');
+
+module.exports = {
+  // context: helpers.root()
+  entry: {
+    'polyfills': './src/polyfills.ts',
+    'vendor': './src/vendor.ts',
+    'app': './src/main.ts'
+  },
+
+  resolve: {
+    extensions: [' ', '.ts', '.js', '.css']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['awesome-typescript-loader', 'angular2-template-loader']
+      },
+      {
+        test: /\.html$/,
+        use: [{loader: 'html-loader', options: {minimize: false}}]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        use: ['file-loader?name=assets/[name].[hash].[ext]']
+      },
+      {
+        test: /\.css$/,
+        exclude: helpers.root('src', 'app'),
+        //loader: 'css-loader'
+        loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader' })
+      },
+      {
+        test: /\.css$/,
+        include: helpers.root('src', 'app'),
+        use: ['raw-loader']
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app', 'vendor', 'polyfills']
+    }),
+
+    new ExtractTextPlugin('[name].[hash].css'),
+
+    new webpack.ContextReplacementPlugin(
+     /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+     __dirname
+   ),
+
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
