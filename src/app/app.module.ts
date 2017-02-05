fce5dd8580e360f2fcb955f23fea49656f719103
Kyle Cornelison <Kyle Cornelison>
2017-02-04 22:04:24 -0700

    Added some very basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { NavComponent } from './components/nav/nav.component';
 import { MaterialModule } from '@angular/material';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     MaterialModule.forRoot()
   ],
   declarations: [
     MainComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   bootstrap: [ MainComponent ]
 })
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import '../../../../public/css/styles.css';
+
+@Component({
+  selector: 'sg-home',
+  templateUrl: './home.component.pug',
+  styleUrls: ['./home.component.css']
+})
+
+export class HomeComponent { }
